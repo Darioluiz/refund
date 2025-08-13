b191764aa505df1353427bff4edb027fb59ee1f8
Darioluiz <darioluiztezolin@gmail.com>
2025-08-13 16:32:47 -0300

    fazendo ajustes e partes funcionais do projeto

---
+++ index.js
@@ -0,0 +1,137 @@
+const form = document.querySelector('form')
+const amount = document.getElementById('amount')
+const category = document.getElementById('category')
+const expense = document.getElementById('expense')
+const expenseList = document.querySelector('ul')
+const expenseQuantity = document.querySelector('aside header p span')
+const expenseTotal = document.querySelector('aside header h2')
+
+amount.oninput = () => {
+  let value = amount.value.replace(/\D/g, '')
+
+  value = Number(value) / 100
+  amount.value = FormarCurrentBRL(value)
+}
+
+function FormarCurrentBRL(value) {
+  value = value.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  })
+
+  return value
+}
+
+form.onsubmit = event => {
+  event.preventDefault()
+
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date()
+  }
+
+  expenseAdd(newExpense)
+}
+
+function expenseAdd(newExpense) {
+  try {
+    const expenseItem = document.createElement('li')
+    expenseItem.classList.add('expense')
+
+    const expenseIcon = document.createElement('img')
+    expenseIcon.setAttribute('src', `img/${newExpense.category_id}.svg`)
+    expenseIcon.setAttribute('alt', newExpense.category_name)
+
+    const expenseInf = document.createElement('div')
+    expenseInf.classList.add('expense-info')
+
+    const expenseName = document.createElement('strong')
+    expenseName.textContent = newExpense.expense
+
+    const expenseCategory = document.createElement('span')
+    expenseCategory.textContent = newExpense.category_name
+
+    expenseInf.append(expenseName, expenseCategory)
+
+    const expenseAmount = document.createElement('span')
+    expenseAmount.classList.add('expense-amount')
+
+    const expenseSmall = document.createElement('span')
+    expenseSmall.classList.add('expense-amount')
+    expenseSmall.innerHTML = `<small>R$</small>${newExpense.amount
+      .toUpperCase()
+      .replace('R$', '')}`
+
+    const expenseRemove = document.createElement('img')
+    expenseRemove.setAttribute('src', 'img/remove.svg')
+    expenseRemove.setAttribute('alt', 'remover')
+    expenseRemove.classList.add('remove-icon')
+
+    expenseItem.append(expenseIcon, expenseInf, expenseSmall, expenseRemove)
+    expenseList.append(expenseItem)
+
+    formClear()
+    upgradeList()
+  } catch (error) {
+    alert('Não foi possivel atualizar a lista de despesas.')
+    console.log(error)
+  }
+}
+
+function upgradeList() {
+  try {
+    const items = expenseList.children
+
+    expenseQuantity.textContent = `${items.length} ${
+      items.length > 1 ? 'despesas' : 'despesa'
+    }`
+
+    let total = 0
+
+    for (let item = 0; item < items.length; item++) {
+      let itemAmount = items[item].querySelector('.expense-amount')
+      let value = itemAmount.textContent
+        .replace(/[^\d,]/g, '')
+        .replace(',', '.')
+
+      value = parseFloat(value)
+
+      if (isNaN(value)) {
+        return alert('Não foi possível fazer o calculo')
+      }
+
+      total += Number(value)
+    }
+    const symbolBRL = document.createElement('small')
+    symbolBRL.textContent = 'R$'
+
+    total = FormarCurrentBRL(total).toUpperCase().replace('R$', '')
+
+    expenseTotal.innerHTML = ''
+
+    expenseTotal.append(symbolBRL, total)
+  } catch (error) {
+    alert('Não foi possível carregar o item')
+    console.log(error)
+  }
+}
+
+expenseList.addEventListener('click', function (event) {
+  if (event.target.classList.contains('remove-icon')) {
+    const item = event.target.closest('.expense')
+    item.remove()
+  }
+  upgradeList()
+})
+
+function formClear() {
+  expense.value = ''
+  category.value = ''
+  amount.value = ''
+
+  expense.focus()
+}
